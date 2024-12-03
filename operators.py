ead200ee4450f4bae012ab92ef08e98ccbb19f22
Tawmeen Hasan Hasib <164780505+Tawmeen@users.noreply.github.com>
2024-12-04 02:46:17 +0600

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,22 @@
+#Arithmetic operator
+a=7
+b=9
+c=a+b
+print(c)
+
+#Assignment operators 
+a=4-2 #Assign 4-2 in a
+print(a)
+b=7
+b-=4 #Decrement the value of b by 4 then assign it to b
+print(b)
+
+#Comparison operators
+d=5<2
+print(d)
+e=5!=7 # != means not equal to
+print(e)
+ 
+#Logical operators
+f=True or False
+print(f)
