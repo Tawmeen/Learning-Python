5d587ef8d0d269d93b863b9e5eb7e0f191cdb335
Tawmeen Hasan Hasib <164780505+Tawmeen@users.noreply.github.com>
2024-12-04 02:45:57 +0600

    Add files via upload

---
+++ list_method.py
@@ -0,0 +1,13 @@
+friends=["Apple","Mango",36,42.7,False,"Rizon","Sagor"]
+
+print(friends)
+
+friends.append("Dhrubo")
+
+print(friends)
+
+l1=[25,6,25,8,2,5]
+l1.sort()
+print(l1)
+l1.remove(6)
+print(l1)
