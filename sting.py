2a322f651c4cfeb2ef6a71254f19e8732f404229
Tawmeen Hasan Hasib <164780505+Tawmeen@users.noreply.github.com>
2024-12-04 02:46:36 +0600

    Add files via upload

---
+++ sting.py
@@ -0,0 +1,20 @@
+name="tawmeen"
+
+nameshort=name[0:2]
+
+charactar1=name[3]
+
+print(charactar1)
+
+print(nameshort)
+
+# Nagetive Slicing.
+names="tawmeen"
+
+print(name[-4:-1])
+
+print(name[3:6])
+
+# Slicing with skip value.
+word="amazing"
+print(word[1:6:2])
